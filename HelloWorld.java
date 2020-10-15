ad6ea96aa3bf2d49c9315f0386072987126ddccb
EC2 Default User <ec2-user@ip-172-31-43-163.ap-south-1.compute.internal>
2020-10-15 20:15:10 +0000

    added another line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
   public static void main(String args[]){
      System.out.println("Welcome to DevOps......GitHub with Jenkins");
+     System.out.println("sudheer try to learn DevOps");
   }
 }
